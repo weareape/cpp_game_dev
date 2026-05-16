9db20b70ad1b319edc3d4c62dbbaa4fcf97fd0df
unknown <rydziur@gmail.com>
2026-05-15 18:26:01 -0700

    week 1 assignment final v.01

--- assignments/week_one/README.md
+++ assignments/week_one/README.md
@@ -1,3 +1,8 @@
 Week 1 Assignment - 2026-05-15
 
 Create a binary search tree and print all 'leaf' nodes.
+
+References:
+https://www.youtube.com/watch?v=COZK7NATh4k - Binary search tree - Implementation in C/C++
+https://www.geeksforgeeks.org/cpp/cpp-binary-search-tree/
+https://www.youtube.com/watch?v=PArGIFNfIDk - Print number of leaf nodes(leaves) in Binary Tree
--- assignments/week_one/binary_search_tree.cpp
+++ assignments/week_one/binary_search_tree.cpp
@@ -1,8 +1,72 @@
 #include<iostream>
-#include<vector>
+
+//Storing root node
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+//Function to get new node and setting to root if root is null
+Node* GetNewNode(int data){
+    Node* newNode = new Node();
+    newNode->data = data;
+    newNode->left = newNode->right = nullptr;
+    return newNode;
+}
+
+//Function to insert a node to the tree
+Node* Insert(Node* root,int data){
+    if(root == nullptr) { //if tree is empty insert new node
+        root = GetNewNode(data);
+    }
+    //Insert left is data is lesser
+    else if(data <= root->data){
+        root->left = Insert(root->left, data);
+    }
+    //Insert right if data is greater
+    else{
+        root->right = Insert(root->right, data);
+    }
+    
+    return root;
+
+}
+
+//int leaves = 0;
+
+int CountLeaves(Node *root){
+    if(root == nullptr){
+        return 0;
+    }
+    
+    if(root->left == nullptr && root->right == nullptr){
+            return 1;
+        }
+    
+    return CountLeaves(root->left) + CountLeaves(root->right);    
+    
+}
+
+
 
 int main(){
+    //Creating root node
+    Node* root = nullptr;
+    //Creating list of nodes
+    root = Insert(root, 15);
+    root = Insert(root, 10);
+    root = Insert(root, 20);
+    root = Insert(root, 25);
+    root = Insert(root, 8);
+    root = Insert(root, 12);
+    root = Insert(root, 44);
+    root = Insert(root, 32);
+    root = Insert(root, 80);
 
+    
+    std::cout << CountLeaves(root) << "\n";
+ 
     return 0;
 
 }
