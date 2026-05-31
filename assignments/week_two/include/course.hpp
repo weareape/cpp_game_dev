58b76bb54ec9090bcd89d6249b7b6eaac31a53d8
unknown <rydziur@gmail.com>
2026-05-31 00:29:07 -0700

    saving progress week two assignment

--- assignments/week_two/include/course.hpp
+++ assignments/week_two/include/course.hpp
@@ -12,27 +12,15 @@ class Course{
         std::string m_courseName;
         std::string m_courseDescription;
 
-        //std::unordered_map<std::string, Teacher*> m_Instructor;
-        //std::unordered_map<std::string, Student*> m_students;
         
     public:
         Course(
-            int &COURSESIZECAP,
+            int COURSESIZECAP,
 
             std::string &COURSENAME, 
             std::string &COURSEDESCRIPTION
         );
-            
-            // std::unordered_map<std::string, Teacher* > &INSTRUCTOR,
-            // std::unordered_map<std::string, Student* > &STUDENTS);
 
-        // virtual void GetInstructor() const = 0;
-        // virtual void AddInstructor(std::string &instructor_name, Teacher* instructor) = 0;
-
-        // virtual void GetAllStudents() const = 0;
-        // virtual void AddStudent(std::string &student_name, Student* student) = 0;
-        
-        // virtual void GetCourseInfo() const = 0;
         void GetCourseInfo() {
             std::cout << "Enrollment Cap: " << m_courseSizeCap << "\n";
             std::cout << "Course Name: " << m_courseName << "\n";
